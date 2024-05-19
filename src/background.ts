f444776a670fb8102737f6c6b3c11a5bf6bcebe4
jianliang <jianliangw1@gmail.com>
2024-05-19 23:11:30 +0800

    add context menu

--- src/background.ts
+++ src/background.ts
@@ -1,2 +1,21 @@
-export {}
-console.log("HELLO WORLD FROM BGSCRIPTS")
+import { browser } from 'webextension-polyfill-ts';
+
+const MENU_ITEM_ID_SELECTION = 'selection'
+
+browser.contextMenus?.create({
+    id: MENU_ITEM_ID_SELECTION,
+    title: 'Read this',
+    contexts: ['selection'],
+})
+
+browser.contextMenus?.onClicked.addListener(async function (info) {
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+
+    console.log('[background.js', 'contextMenus onClicked', info);
+    if (info.menuItemId === MENU_ITEM_ID_SELECTION) {
+    tab.id &&
+    (await browser.tabs.sendMessage(tab.id, {
+        type: MESSAGE_TYPE_MENU_CLICKED,
+    }));
+}
+});
